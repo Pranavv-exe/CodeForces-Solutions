851e6e933f032dbf2ccc1046f9be5a5eed6b1151
Dev-Vasudevan <119096133+Dev-Vasudevan@users.noreply.github.com>
2023-08-12 02:27:43 +0530

    Add files via upload

---
+++ 1856A/1856A.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include <string> 
+using namespace std;
+int main (){
+    int n,i;
+    string output="";
+    cin>>n;
+    for (i=0;i<n;i++){
+        int n1,i1,max_number=0,current_num,iter=0;
+        string ans;
+        cin>>n1;
+
+        for (i1=0;i1<n1;i1++){
+            
+            cin>>current_num;
+
+            if (max_number<=current_num )
+                max_number= current_num;
+
+            
+            else{
+                iter=max_number;
+            }
+
+        }
+        ans = to_string(iter); 
+        output=output + ans;
+        if (i!=(n-1)){
+            output=output +"\n";
+        }
+        
+    }
+    cout<<output;
+
+}
